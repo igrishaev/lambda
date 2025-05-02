7dcd232a6bd8acbdf608f8830d44731ba037b4b9
Ivan Grishaev <ivan@grishaev.me>
2025-05-02 10:50:49 +0300

    add futures

--- src/lambda/main.clj
+++ src/lambda/main.clj
@@ -6,26 +6,53 @@
   (:require
    [lambda.log :as log]
    [lambda.api :as api]
-   [lambda.error :as e]))
+   [lambda.error :as e])
+  (:import
+   (java.util.function Function
+                       Supplier)
+   (java.util.concurrent CompletableFuture)
+   (java.util.concurrent Executors)))
+
+
+(set! *warn-on-reflection* true)
+
+
+(defmacro supplier [& body]
+  `(reify Supplier
+     (get [this#]
+       ~@body)))
+
+
+(defmacro function [[bind] & body]
+  `(reify Function
+     (apply [this# ~bind]
+       ~@body)))
 
 
 (defn run [fn-event]
 
   (while true
-
     (let [{:keys [headers body]}
           (api/next-invocation)
 
           request-id
-          (get headers :lambda-runtime-aws-request-id)
+          (get headers :lambda-runtime-aws-request-id)]
 
-          [e response]
-          (e/with-safe
-            (fn-event body))]
+      (-> (CompletableFuture/supplyAsync
+           (supplier
+             (fn-event body)))
+          (.thenApplyAsync
+           (function [response]
+             (api/invocation-response request-id response)))
+          (.exceptionally
+           (function [e]
+             (log/errorf "Event error, request ID: %s" request-id)
+             (log/exception e)
+             (api/invocation-error request-id e))))))
 
-      (if e
-        (do
-          (log/errorf "Event error, request ID: %s" request-id)
-          (log/exception e)
-          (api/invocation-error request-id e))
-        (api/invocation-response request-id response)))))
+  #_
+  (let [executor
+        (Executors/newFixedThreadPool 4)]
+
+
+    ))
