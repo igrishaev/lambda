9b69706d0edca77d743a06118e8d99a51899a0fd
Ivan Grishaev <ivan@grishaev.me>
2025-05-22 10:14:25 +0300

    better lambda

--- env/demo3/src/demo3/main.clj
+++ env/demo3/src/demo3/main.clj
@@ -17,29 +17,68 @@
   (let [-state (atom {})]
     (reify ICounter
       (-inc-page [this uri]
-        (swap! -state update uri (fnil inc 0)))
+        (swap! -state update uri (fnil inc 0))
+        this)
       (-get-page [this uri]
         (get @-state uri 0))
       (-stats [this]
         @-state))))
 
 
-(defn handler-index [request counter]
+(def HELP "
+Hi!
+
+This is my lambda function for public tests. It's written in Clojure and
+compiled with native image. The output binary file is run in bare AWS
+environment (language-agnostic). The file communicates with AWS Lambda runtime
+directly.
+
+The Lambda uses Stuart Sierra's Component library and a Ring middleware that
+turns AWS messages into Ring-compatible maps. See the source code of this demo:
+
+https://github.com/igrishaev/lambda/blob/master/env/demo3/src/demo3/main.clj
+
+This is what it can do:
+
+- GET /
+  Show this message;
+
+- GET /stats
+  Return statistics about how many times pages were seen;
+
+- GET /<whatever>
+  Increase in-memory counter for the current page.
+
+You can benchmark this lambda as follows to measure RPS:
+
+ab -n 1000 -c 200 -l https://kpryignyuxqx3wwuss7oqvox7q0yhili.lambda-url.us-east-1.on.aws/
+
+~Ivan
+")
+
+
+(defn handler-info [request]
+  {:status 200
+   :body HELP
+   :headers {"content-type" "text/plain"}})
+
+
+(defn handler-stats [request counter]
   {:status 200
    :body {:stats (-stats counter)}})
 
 
-(defn handler-hello [request]
-  {:status 200
-   :body {:page "hello"}})
-
-
 (defn response-default [request counter]
   (let [{:keys [uri]}
-        request]
-    (-inc-page counter uri)
+        request
+
+        times
+        (-> counter
+            (-inc-page uri)
+            (-get-page uri))]
+
     {:status 200
-     :body {:seen true
+     :body {:times times
             :uri uri}}))
 
 
@@ -50,10 +89,10 @@
       (case [request-method uri]
 
         [:get "/"]
-        (handler-index request counter)
+        (handler-info request)
 
-        [:get "/hello"]
-        (handler-hello request)
+        [:get "/stats"]
+        (handler-stats request counter)
 
         (response-default request counter)))))
 
